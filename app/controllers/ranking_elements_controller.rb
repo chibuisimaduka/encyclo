57bffb89c44371572060893425cbe5544102acd8
pascalr <pascal.rainville@gmail.com>
2012-01-11 13:23:58 -0500

    Fixed ranking for jquery.

--- app/controllers/ranking_elements_controller.rb
+++ app/controllers/ranking_elements_controller.rb
@@ -1,29 +1,21 @@
 class RankingElementsController < ApplicationController
  
-  before_filter :get_ranking, :only => ["update"]
-
   # TODO: Test this..
   def rank
+    @ranking = Ranking.find_or_create_by_user_id_and_tag_id(current_user.id, params[:tag_id])
     raise "missing record_id" unless params[:record_id]
 
     @elem = @ranking.ranking_elements.find_or_initialize_by_record_id(params[:record_id])
-    # TODO: Add column num_votes
-    @elem.record.rank = ((@elem.record.rank * @elem.record.num_votes) - (@elem.rating || 0) + params[:rating].to_f) / (@elem.record.num_votes += 1)
+    @elem.record.update_attribute :rank, (((@elem.record.rank || 0) * @elem.record.num_votes) - (@elem.rating || 0) + params[:rating].to_f) / (@elem.record.num_votes += 1)
     @elem.rating = params[:rating]
-	
-    render :update do |page|
-      page.replace_html "rating_for_tag_#{params[:tag_id]}_record_#{params[:record_id]}", :partial => "helpers/rating_list", 
-        :locals => {:ranking_element => @elem, :tag_id => params[:tag_id], :record_id => params[:record_id]}
-      #page.visual_effect :highlight, 'user_list'
-    end
+    @elem.save!
   end
 
   # TODO: Test this..
   def destroy
     @record = RankingElement.find params[:id]
-    @record.record.rank = ((@record.record.rank * @record.record.num_votes) - @record.rating) / (@record.record.num_votes -= 1)
 	 if @record
-	   @record.remove_from_list
+      @record.record.update_attribute :rank, ((@record.record.rank * @record.record.num_votes) - @record.rating) / (@record.record.num_votes -= 1) if @record.record.rank
 	   @record.destroy
 	 end
 	 redirect_to :back
@@ -61,12 +53,6 @@ class RankingElementsController < ApplicationController
             :locals => {:ranking_id => params[:ranking_id], :record_id => record_id, :position => index+1}
       end
     end
-  end
-
-private
-
-  def get_ranking
-    @ranking = Ranking.find_or_create_by_user_id_and_tag_id(current_user.id, params[:tag_id])
   end
 
 end
