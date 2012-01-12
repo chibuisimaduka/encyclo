278f7717182b8f5c5dfc385eab4203d73ad4bc8b
pascalr <pascal.rainville@gmail.com>
2012-01-12 00:22:32 -0500

    Little more touch to images stuff.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,10 @@
 class Image < ActiveRecord::Base
+  attr_accessible :remote_image_url
   belongs_to :entity
   mount_uploader :image, ImageUploader
+  #mount_uploader :image, ImageUploader, :mount_as => :url
+
+  validates_presence_of :image
+
+  before_destroy :remove_image!
 end
