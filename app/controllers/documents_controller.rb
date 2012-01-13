e5984fa9f1cbc290e4024ea3d6fd76f9800e2671
pascalr <pascal.rainville@gmail.com>
2012-01-12 22:58:00 -0500

    Getting documents content.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,5 +1,7 @@
 class DocumentsController < ApplicationController
 
+  require 'open-uri'
+
   def show
     @document = Document.find(params[:id])
 	 #@document.content = fetch @document.source
@@ -8,18 +10,17 @@ class DocumentsController < ApplicationController
 
   def create
     @entity = Entity.find(params[:entity_id])
-	 @document = @entity.documents.create!(params[:document])
-	 #@document.content = fetch @document.source
-	 #@document.save!
+	 @document = @entity.documents.create(params[:document])
+    uri = URI.parse(URI.escape(URI.unescape(@document.source)))
+    @document.name = "#{uri.host} - #{@entity.name}"
+	 @document.content = fetch(uri)
+	 @document.save!
 	 redirect_to @entity
   end
 
 private
-  def fetch(raw_url)
-    require 'net/http'
-    require 'uri'
-    url = URI.parse(raw_url)
-    res = Net::HTTP.start(url.host, url.port) {|http|
+  def fetch(uri)
+    res = Net::HTTP.start(uri.host, uri.port) {|http|
       http.get('/index.html')
     }
     res.body
