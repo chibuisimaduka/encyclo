698a6f51a388021923dd3c66e40fadec73985e9d
pascalr <pascal.rainville@gmail.com>
2012-01-27 10:50:56 -0500

    Did little things and started class diagram.

--- app/models/predicate.rb
+++ app/models/predicate.rb
@@ -4,10 +4,18 @@ class Predicate < ActiveRecord::Base
 
   validates_presence_of :component_id
   validates_presence_of :entity_id
+  validates_presence_of :value
   validate :value_type
 
 private
   def value_type
-    errors.add(:value, "must reference a valid entity.") if Entity.find_by_name(self.value).blank?
+    case self.component.component_type
+      when ComponentType::ENTITY; errors.add(:value, "must reference a valid entity.") if Entity.find_by_name(self.value).blank?
+      when ComponentTYPE::ENTITIES
+        JSON.parse(self.value).each do |val|
+          errors.add(:value, "every one must reference a valid entity.") if Entity.find_by_name(val).blank?
+        end
+    end
+    
   end
 end
