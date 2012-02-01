2a965838274c1efc95b6c9d34856ebcd640a96f2
pascalr <pascal.rainville@gmail.com>
2012-02-01 13:35:05 -0500

    Not much..

--- app/controllers/associations_controller.rb
+++ app/controllers/associations_controller.rb
@@ -1,13 +1,6 @@
 class AssociationsController < ApplicationController
   respond_to :html, :json
   
-  def update
-    @association = Predicate.find_or_create_by_component_id_and_entity_id(params[:component_id], params[:entity_id])
-    @association.add_value(params[:index].to_i, params[:association][:"value_#{params[:index]}"])
-    @association.save
-    respond_with @association
-  end
-
   def create
     @association = Association.new(params[:association])
     @association.save!
