68ece7b0581f41d17040fa0af299c5b696287d3b
pascalr <pascal.rainville@gmail.com>
2012-01-31 20:45:01 -0500

    Upgraded map_all. Using block instead of column.

--- lib/active_record_utils.rb
+++ lib/active_record_utils.rb
@@ -1,11 +1,11 @@
 module ActiveRecordUtils
 
-  def collect_all(parent_name, column=nil)
+  def collect_all(parent_name, include_self=true, &block)
     # FIXME: Should always be a block instead of column. &:name instead of :name
     parent = self.send(parent_name)
-    values = column ? self.send(column) : self
+    values = include_self ? (block_given? ? yield(self) : self.send(parent_name)) : parent.blank? ? [] : (block_given? ? yield(parent) : parent.send(parent_name))
     values = values.blank? ? [] : [values] unless values.is_a? Array
-    parent.blank? ? values : values + parent.collect_all(parent_name, column)
+    parent.blank? ? values : values + parent.collect_all(parent_name, &block)
   end
 
   alias map_all collect_all
