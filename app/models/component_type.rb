7abe3251b7acd40f48d6028ef55e145ade77b2f0
pascalr <pascal.rainville@gmail.com>
2012-02-06 19:43:32 -0500

    Fixup

--- app/models/component_type.rb
+++
@@ -1,9 +0,0 @@
-class ComponentType
-  INTEGER = 1
-  BOOLEAN = 2
-  FLOAT = 3
-  ENTITY_REF = 4
-  RANGE = 5
-  
-  ALL = {INTEGER => "integer", BOOLEAN => "boolean", FLOAT => "float", ENTITY_REF => "entity_ref", RANGE => "range"}
-end
