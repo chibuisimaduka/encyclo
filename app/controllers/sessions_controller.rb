cfc6d8c0aab54d0acfcfbd827c506d318cb849b3
pascalr <pascal.rainville@gmail.com>
2012-02-09 14:27:35 -0500

    Login in navigation menu.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,6 @@ class SessionsController < ApplicationController
   
   respond_to :html, :json
 
-  def new
-  end
-
   def update
     value = params[params[:value_key]]
     session[params[:session_key]].update_attributes value
@@ -15,16 +12,15 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:email], params[:password])
     if user
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
+      redirect_to :back, :notice => "Logged in!"
     else
-      flash.now.alert = "Invalid email or password"
-      render "new"
+      redirect_to :back, :notice => "Invalid email or password."
     end
   end
 
   def destroy
     session[:user_id] = nil
-	 redirect_to root_url, :notice => "Logged out!"
+	 redirect_to :back, :notice => "Logged out!"
   end
 
 end
