45cbe5e8d4b13163c4f71aa8bf2770158f9c3fe7
pascalr <pascal.rainville@gmail.com>
2012-02-10 13:37:19 -0500

    Did important stuff that i dont remember. Bug fixes I think.

--- app/controllers/possible_name_spellings_controller.rb
+++ app/controllers/possible_name_spellings_controller.rb
@@ -3,7 +3,6 @@ class PossibleNameSpellingsController < ApplicationController
   def create
     @name = Name.find(params[:name_id])
     @name.set_value(params[:possible_name_spelling][:spelling], current_user)
-    redirect_to :back
   end
 
 end
