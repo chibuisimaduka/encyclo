192d1793b0d0e0289a51ba7b23fb7fcb4413b18f
pascalr <pascal.rainville@gmail.com>
2012-02-10 23:53:49 -0500

    Adding images inline.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,7 +3,10 @@ class ImagesController < ApplicationController
   def create
     @entity = Entity.find(params[:entity_id])
     @entity.images.create(params[:image].merge(:source => params[:image][:remote_image_url]))
-    redirect_to @entity
+    respond_to do |format|
+      format.html { redirect_to @entity }
+      format.js
+    end
   end
 
 end
