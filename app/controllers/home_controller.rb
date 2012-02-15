02b44402555b62a7e9579a9092940de2e5c358e0
pascalr <pascal.rainville@gmail.com>
2012-02-15 10:59:42 -0500

    home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
 
   def index
-    @entities = Entity.safe_find([869, 862, 1154, 880, 864])
+    # TODO: Use statistics to find most appropriate entities instead of static entities.
+    # Using conditions so it does not raise an exception if one of the entity is missing.
+    @entities = Entity.all(:conditions => {:id => [869, 862, 1154, 880, 864]})
   end
 
 end
