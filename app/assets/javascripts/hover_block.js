ea34c3ce6effb3b049b45f961c02b9a08daa14d9
pascalr <pascal.rainville@gmail.com>
2012-02-15 16:08:28 -0500

    Upgraded hover block in progress.

--- app/assets/javascripts/hover_block.js
+++ app/assets/javascripts/hover_block.js
@@ -1,7 +1,13 @@
 //= require livequery
 
 $(document).ready(function() {
-  $('.hover_block').livequery('hover', function() {
+  $('.hover_block').livequery('mouseenter', function() {
+    if (window.last_overed_element && window.last_overed_element == $(this)) {
+      // Nothing to be done..
+    } else if (window.last_overed_element) {
+      toggleElements(window.last_overed_element.children('.hover_block_hidden').add(window.last_overed_element.find('[block_id="' + window.last_overed_element.attr('id') + '"]')));
+    }
     toggleElements($(this).children('.hover_block_hidden').add($(this).find('[block_id="' + $(this).attr('id') + '"]')));
+    window.last_overed_element = $(this);
   })
 });
