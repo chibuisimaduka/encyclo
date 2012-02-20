b9a4daf93ed66be93995183a9729c3b8b5f496b2
pascalr <pascal.rainville@gmail.com>
2012-02-20 11:25:52 -0500

    Components working perfectly.

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -2,7 +2,6 @@ class ComponentsController < ApplicationController
 
   def create
     @component = Component.new(params[:component])
-    @component.user_id = current_user.id
     if params["commit"] == "Change parent"
       @entities = Entity.find_all_by_id_or_by_name(params[:component][:associated_entity_id], params[:name], current_language)
       if @entities.size != 1
@@ -11,6 +10,10 @@ class ComponentsController < ApplicationController
       else
         @entity = @entities.first
         @entity.parent_id = params[:component][:entity_id]
+        if @entity.component_id
+          @component = @entity.component
+          @component.entity_id = @entity.parent_id
+        end
       end
     else
       @entity = Entity.new(parent_id: params[:component][:entity_id])
@@ -18,16 +21,17 @@ class ComponentsController < ApplicationController
       @name = @entity.names.build(language_id: current_language.id)
       @name.set_value(params[:name], current_user)
     end
+    @component.user_id = current_user.id
     @entity.component = @component
-    #begin
+    begin
       Component.transaction do
-        @component.save!
         @entity.recalculate_ancestors(true)
+        @component.save!
         flash[:notice] = 'Entity was succesfully transform into an entity.'
       end
-    #rescue
-    #  flash[:notice] = "An error has occured while creating component"
-    #end
+    rescue
+      flash[:notice] = "An error has occured while creating component"
+    end
     redirect_to params[:show] ? @entity : :back
   end
 
