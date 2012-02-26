3e63f1d21ca8903265ac58d1e1150fe2a0fe0054
pascalr <pascalr@pascalr.(none)>
2012-02-26 18:34:27 -0500

    Database/deploy in progress.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,12 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_ruby_string, 'ruby-1.9.2-head@rails313'
+set :rvm_type, :user
+
+require "bundler/capistrano"
+
+require "capistrano_database"
+
 set :application, "enyclo"
 set :repository,  "git://github.com/pascalr/encyclo.git"
 
@@ -6,6 +15,7 @@ set :scm, :git
 
 set :deploy_to, "/var/www/encyclo"
 
+set :user, "pascalr"
 set :use_sudo, false
 
 role :web, "216.221.61.38"                          # Your HTTP server, Apache/etc
@@ -13,14 +23,21 @@ role :app, "216.221.61.38"                          # This may be the same as yo
 role :db,  "216.221.61.38", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa_admin")]
+
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do
+    run "#{try_sudo} /opt/nginx/sbin/nginx"
+  end
+  task :stop do
+    run "if [ -s /opt/nginx/logs/nginx.pid ]; then #{try_sudo} /opt/nginx/sbin/nginx -s stop; fi"
+  end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "if [ -s /opt/nginx/logs/nginx.pid ]; then #{try_sudo} /opt/nginx/sbin/nginx -s stop; fi"
+    run "#{try_sudo} /opt/nginx/sbin/nginx"
+  end
+end
