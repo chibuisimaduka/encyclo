c9659545eec4350b8204cd5c8835046af36ffca4
Pascal Rainville <pascal.rainville@gmail.com>
2012-03-02 11:50:20 -0500

    Fixed updating document type and polymorphic_path.

--- app/controllers/possible_document_types_controller.rb
+++ app/controllers/possible_document_types_controller.rb
@@ -2,8 +2,11 @@ class PossibleDocumentTypesController < ApplicationController
 
   def create
     @document = Document.find(params[:document_id])
-    @possible_document_type = @document.possible_document_types.find_or_create_by_document_type_id(params[:possible_document_type][:document_type_id])
+    @possible_document_type = @document.possible_document_types.find_or_initialize_by_document_type_id(params[:possible_document_type][:document_type_id])
     EditRequest.update(@possible_document_type, @document.possible_document_types, current_user)
+    unless @document.possible_document_types.include?(@possible_document_type)
+      @document.possible_document_types << @possible_document_type 
+    end
   end
 
 end
