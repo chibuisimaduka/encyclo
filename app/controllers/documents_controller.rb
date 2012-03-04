61eca8e22cd057f26c3fc7ff53f6e65d28f87d73
Pascal Rainville <pascal.rainville@gmail.com>
2012-03-03 21:10:44 -0500

    Documents are now pretty not bad..

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -20,8 +20,8 @@ class DocumentsController < ApplicationController
   end
     
   def upload
-    @entity = Entity.find(params[:entity_id])
-    @document = Document.new
+    @entity = Entity.find(params[:entity_id]) if params[:entity_id]
+    @document = Document.new(params[:document])
     # TODO: Add as image if it is an image.
     extension = File.extname(params[:file].original_filename)
     content = params[:file].read
@@ -35,8 +35,8 @@ class DocumentsController < ApplicationController
       @document.name = params[:file].original_filename
       @document.language_id = current_language.id
       @document.user_id = current_user.id
-      @document.description = content[0..(Document::MAX_DESCRIPTION_LENGTH-1)]
-      if @entity.documents << @document
+      @document.description = content[0..(Document::MAX_DESCRIPTION_LENGTH-1)] if @entity
+      if @entity ? @entity.documents << @document : @document.save
         redirect_to @document
       else
         redirect_to :back, :alert => "An error has occured while creating the document."
