c7c9f9e11baaafb38adf7e7dd8621f205e262178
Pascal Rainville <pascal.rainville@gmail.com>
2012-03-04 23:24:08 -0500

    Documents are not bad.

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,8 +1,9 @@
 module DocumentsHelper
 
   def documents_for_entity(entity, page, per_page)
-    documents = DeleteRequest.alive_scope(entity.documents.where(language_id: current_language.id, component_id: nil),
-      current_user).paginate(page: params[:document_page], per_page: per_page)
+    definition_document = entity.documents.find_by_name("definition")
+    documents = DeleteRequest.alive_scope(definition_document.documents,#.where(language_id: current_language.id),
+      current_user).paginate(page: params[:document_page], per_page: per_page) if definition_document
   end
 
 end
