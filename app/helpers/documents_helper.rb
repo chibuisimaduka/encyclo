65dccd42cab31adba99e308fafb7afac73ad90f0
Pascal Rainville <pascal.rainville@gmail.com>
2012-03-17 15:18:25 -0400

    Little things..

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -2,7 +2,7 @@ module DocumentsHelper
 
   def documents_for_entity(entity, page, per_page)
     definition_document = entity.documents.find_by_name("definition")
-    documents = DeleteRequest.alive_scope(definition_document.documents,#.where(language_id: current_language.id),
+    documents = DeleteRequest.alive_scope(definition_document.documents.where(language_id: current_language.id),
       current_user).paginate(page: params[:document_page], per_page: per_page) if definition_document
   end
 
