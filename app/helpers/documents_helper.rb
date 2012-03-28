4e2869882e257d53a1e128815d1372ea3dc5dba1
Pascal Rainville <pascal.rainville@gmail.com>
2012-03-28 19:06:36 -0400

    Fixed definitions showing twice.

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -6,4 +6,12 @@ module DocumentsHelper
       current_user).paginate(page: params[:document_page], per_page: per_page) if definition_document
   end
 
+  def ancestors_documents(entity)
+    documents = {}
+    entity.ancestors.each do |e|
+      e.listing_documents.each {|d| documents[d.name] ||= d }
+    end
+    documents.values
+  end
+
 end
