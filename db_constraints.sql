2c6689cc3b14545aa4250623d2b527939a939e95
Pascal Rainville <pascal.rainville@gmail.com>
2012-03-30 18:12:13 -0400

    Cleaned up main folder.

--- db_constraints.sql
+++
@@ -1,4 +0,0 @@
-Alter TABLE association_definitions MODIFY entity_id int(11) NOT NULL;
-Alter Table association_definitions ADD FOREIGN KEY (entity_id) REFERENCES entities(id);
--- Alter TABLE Persons ADD UNIQUE (column_blah)
--- Alter Table Orders ADD FOREIGN KEY (P_Id) REFERENCES Persons(P_Id)
