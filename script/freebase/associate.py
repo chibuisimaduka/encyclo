515e2bb2d63e6dc29d53b8b8b6e4eb8f8f81dbde
Pascal Rainville <pascal.rainville@gmail.com>
2012-04-19 12:50:53 -0400

    Association theorically works.

--- script/freebase/associate.py
+++ script/freebase/associate.py
@@ -6,18 +6,19 @@ import sys
 import utils
 
 # Command line arguments:
-if len(sys.argv) != 4: raise RuntimeError("You must pass the association_definition_id, the property_id and the file name!")
+if len(sys.argv) != 5: raise RuntimeError("You must pass the association_definition_id, is_reversed, the property_id and the file name!")
 association_definition_id = sys.argv[1]
-property_id = sys.argv[2] # Ex: /film/film/starring
-filename = sys.argv[3]
+association_reversed = (sys.argv[2] == 'true')
+property_id = sys.argv[3] # Ex: /film/film/starring
+filename = sys.argv[4]
 
 parent_id, associated_parent_id = utils.query_sql("SELECT entity_id,associated_entity_id FROM association_definitions WHERE id=" + association_definition_id)[0]
 
 def get_entities_dict(parent_id):
   return dict(utils.query_sql("select freebase_id,id from entities where freebase_id IS NOT NULL and parent_id=" + str(parent_id)))
 
-entities = get_entities_dict(parent_id)
-associated_entities = get_entities_dict(associated_parent_id)
+entities = get_entities_dict(parent_id if not association_reversed else associated_parent_id)
+associated_entities = get_entities_dict(associated_parent_id if not association_reversed else parent_id)
 
 sys.stderr.write("Starting to fetch entries..")
 
@@ -29,7 +30,6 @@ for line in open(filename, 'r'):
   if entity_id in entities and line.endswith('\t'):
     associated_entity_id = line[(line[:-1].rindex('\t')+1):-1]
     if line[line.index('\t')+1:].startswith(property_id) and associated_entity_id in associated_entities:
-      import pdb; pdb.set_trace()
       statement += "(9,'" + association_definition_id + "','" + str(entities[entity_id]) + "','" + str(associated_entities[associated_entity_id]) + "'),"
       num_entries += 1
       if (num_entries % 500 == 0): sys.stderr.write("Entered 500 entities!")
