83f7b8fdfbd34e317a4a7dae0669b00fa908b33d
Pascal Rainville <pascal.rainville@gmail.com>
2012-04-25 23:08:58 -0400

    Pagination updates url, but reloads.. damn.

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,7 +1,9 @@
 $(function() {
   $(".pagination a").live("click", function() {
     $(this).parent().html("Page is loading...");
-    $.getScript(this.href);
+    // TODO: A hack that set the attributes on location.hash. For now, just reload the page.
+    //$.getScript(this.href); 
+    window.location.href = this.href; 
     return false;
   });
 });
